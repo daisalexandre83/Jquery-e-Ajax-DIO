5e98786c99394e62795126f8ec3781249a840221
Daiane de Souza Alexandre <dsalexandre83@gmail.com>
2021-10-05 22:11:35 -0300

    Inserção do HTML,CSS e Javascript.

---
+++ js/main.js
@@ -0,0 +1,26 @@
+function consultarCep() {
+    var cep = document.getElementById("cep").value;
+    var url = "https://viacep.com.br/ws/" + cep +"/json/";
+    console.log(url);
+    $.ajax({
+        url: url,
+        type:"GET",
+        success:function(response){
+            console.log(response);
+           $("#logradouro").html(response.logradouro);
+            $("#bairro").html(response.bairro);
+           $("#localidade").html(response.localidade);
+           $("#uf").html(response.uf); 
+           $("#titulo_cep").html("CEP " + response.cep);
+          
+          
+           /*  document.getElementById("logradouro").innerHTML=response.logradouro;
+            document.getElementById("bairro").innerHTML=response.bairro;
+            document.getElementById("localidade").innerHTML=response.localidade;
+            document.getElementById("uf").innerHTML=response.uf; */
+        }
+    })
+}
+ $(function(){
+    $(".cep").hide();
+});  
